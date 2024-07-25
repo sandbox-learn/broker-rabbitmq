6195151f7c16ca9e3c777eba21d0a9fdf06879e3
aassalax <aassali.dev@gmail.com>
2024-07-25 16:02:43 +0200

    command line runner

--- src/main/kotlin/com/sandbox/rabbitmq/broker/BrokerApplication.kt
+++ src/main/kotlin/com/sandbox/rabbitmq/broker/BrokerApplication.kt
@@ -1,10 +1,25 @@
 package com.sandbox.rabbitmq.broker
 
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BrokerApplication
+class BrokerApplication : CommandLineRunner {
+	override fun run(vararg args: String) {
+		Thread {
+			while (true) {
+				try {
+					Thread.sleep(1000)
+				} catch (e: InterruptedException) {
+					Thread.currentThread().interrupt()
+					break
+				}
+			}
+		}.start()
+	}
+
+}
 
 fun main(args: Array<String>) {
 	runApplication<BrokerApplication>(*args)
